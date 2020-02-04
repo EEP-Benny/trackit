478bc1f6afdb5026ff1c15551dc107a47c8344c0
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-02-04 22:12:42 +0100

    Animations for enter and leave

--- trackit/src/app/app.component.ts
+++ trackit/src/app/app.component.ts
@@ -2,11 +2,33 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IEntry, IEntryWithId } from './interfaces/IEntry';
 import { EntryService } from './services/entry.service';
+import { trigger, transition, style, animate, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('items', [
+      transition(':enter', [
+        style({ opacity: 0, height: 0 }),
+        animate('250ms cubic-bezier(0.0, 0.0, 0.2, 1)',
+          style({ opacity: 1, height: '*' }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, height: '*' }),
+        animate('150ms cubic-bezier(0.4, 0.0, 1, 1)',
+          style({
+            opacity: 0, height: 0
+          }))
+      ])
+    ]),
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(25, animateChild()))
+      ]),
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
