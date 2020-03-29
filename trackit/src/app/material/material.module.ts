717f4c170c8380f0e0086b384bdee288490b1f55
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2020-03-29 20:07:48 +0200

    Angular and package updates

--- trackit/src/app/material/material.module.ts
+++ trackit/src/app/material/material.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatDatepickerModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatListModule,
-  MatNativeDateModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatSidenavModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
