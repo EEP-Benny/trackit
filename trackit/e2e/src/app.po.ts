59aece30803e20ce8e712481397fcc62f016f9e7
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2020-08-13 18:22:58 +0200

    Make E2E tests work

--- trackit/e2e/src/app.po.ts
+++ trackit/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('ti-root mat-toolbar .app-title')).getText();
   }
 }
