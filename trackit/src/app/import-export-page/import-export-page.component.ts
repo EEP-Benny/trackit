c8256efff6f5ac8415d8c5156c97ea7e81198c06
Benjamin Hogl <benjamin.hogl@jambit.com>
2021-01-19 19:32:51 +0100

    Import functionality

--- trackit/src/app/import-export-page/import-export-page.component.ts
+++ trackit/src/app/import-export-page/import-export-page.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { IEntry } from '../interfaces/IEntry';
 import { saveAs } from 'file-saver';
-import { csvFormat } from 'd3-dsv';
+import { csvFormat, csvParseRows } from 'd3-dsv';
 import { DexieService } from '../services/dexie.service';
+import { EntryService } from '../services/entry.service';
 
 type ExportData = { entries: IEntry[]; date: Date };
+type ImportData = { entries: IEntry[]; filename: string };
 
 @Component({
   selector: 'ti-import-export-page',
@@ -12,9 +15,18 @@ type ExportData = { entries: IEntry[]; date: Date };
   styleUrls: ['./import-export-page.component.css'],
 })
 export class ImportExportPageComponent implements OnInit {
+  @ViewChild('importSuccessSnackBar')
+  importSuccessSnackBarTemplate: TemplateRef<any>;
+
   exportData: ExportData;
 
-  constructor(private readonly dexieService: DexieService) {}
+  importData: ImportData;
+
+  constructor(
+    private readonly dexieService: DexieService,
+    private readonly entryService: EntryService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {}
 
@@ -29,5 +41,45 @@ export class ImportExportPageComponent implements OnInit {
     const csvContent = csvFormat(exportData.entries, ['timestamp', 'value']);
     const csvBlob = new Blob([csvContent], { type: 'text/csv' });
     saveAs(csvBlob, filename);
+  }
+
+  async uploadFile(file?: File) {
+    if (!file) {
+      return;
+    }
+    this.importData = {
+      filename: file.name,
+      entries: await this.readEntriesFromFile(file),
+    };
+  }
+
+  async readEntriesFromFile(file: File) {
+    const csvContent = await file.text();
+    const csvRows = csvParseRows(csvContent);
+    const headerRow = csvRows.shift(); // gets the first row and removes it from the array
+    const timestampIndex = headerRow.indexOf('timestamp');
+    const valueIndex = headerRow.indexOf('value');
+    if (timestampIndex < 0 || valueIndex < 0) {
+      return; // some columns are missing
+    }
+    const entries: IEntry[] = csvRows
+      .map((row) => ({
+        timestamp: new Date(row[timestampIndex]),
+        value: +row[valueIndex],
+      }))
+      .filter((entry) => isFinite(entry.value));
+    return entries;
+  }
+
+  async import(overwrite: boolean) {
+    if (overwrite) {
+      // TODO: Add confirmation dialog
+      await this.dexieService.entries.clear();
+    }
+    await this.dexieService.entries.bulkAdd(this.importData.entries);
+    this.entryService.fetchEntriesFromDb();
+    this.snackBar.openFromTemplate(this.importSuccessSnackBarTemplate, {
+      duration: 3000,
+    });
   }
 }
--- trackit/src/app/material/material.module.ts
+++ trackit/src/app/material/material.module.ts
@@ -7,36 +7,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+const modules = [
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+];
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatListModule,
-    MatSidenavModule,
-    MatTabsModule,
-  ],
-  exports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatListModule,
-    MatSidenavModule,
-    MatTabsModule,
-  ],
+  imports: [CommonModule, ...modules],
+  exports: [...modules],
 })
 export class MaterialModule {}
