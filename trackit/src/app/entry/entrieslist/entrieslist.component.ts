b8237a2f7100cb391ac161ba3e267d4618f0d8e8
Benjamin Hogl <benjamin.hogl@jambit.com>
2021-01-25 16:49:29 +0100

    Replace mat-list with table for better alignment

--- trackit/src/app/entry/entrieslist/entrieslist.component.ts
+++ trackit/src/app/entry/entrieslist/entrieslist.component.ts
@@ -18,20 +18,17 @@ import { Observable } from 'rxjs';
   animations: [
     trigger('items', [
       transition(':enter', [
-        style({ opacity: 0, height: 0 }),
+        style({ opacity: 0, height: 0, lineHeight: 0 }),
         animate(
           '250ms cubic-bezier(0.0, 0.0, 0.2, 1)',
-          style({ opacity: 1, height: '*' })
+          style({ opacity: 1, height: '*', lineHeight: '*' })
         ),
       ]),
       transition(':leave', [
-        style({ opacity: 1, height: '*' }),
+        style({ opacity: 1, height: '*', lineHeight: '*' }),
         animate(
           '150ms cubic-bezier(0.4, 0.0, 1, 1)',
-          style({
-            opacity: 0,
-            height: 0,
-          })
+          style({ opacity: 0, height: 0, lineHeight: 0 })
         ),
       ]),
     ]),
