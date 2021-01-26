a4a92791d1f2b98f3dfeca09d076f58da66f097a
Benjamin Hogl <benjamin.hogl@jambit.com>
2021-01-26 17:07:59 +0100

    Strong typing for form

--- trackit/src/app/entry/entry-dialog/entry-dialog.component.ts
+++ trackit/src/app/entry/entry-dialog/entry-dialog.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { IFormGroup, IFormBuilder } from '@rxweb/types';
 import { EntryService } from 'src/app/services/entry.service';
+
+interface FormEntry {
+  date: Date;
+  value: string;
+}
 
 @Component({
   selector: 'ti-entry-dialog',
@@ -9,18 +15,22 @@ import { EntryService } from 'src/app/services/entry.service';
   styleUrls: ['./entry-dialog.component.css'],
 })
 export class EntryDialogComponent implements OnInit {
-  form: FormGroup;
+  form: IFormGroup<FormEntry>;
+  formBuilder: IFormBuilder;
   addAnotherControl: FormControl;
 
   constructor(
     private readonly entryService: EntryService,
+    formBuilder: FormBuilder,
     private dialogRef: MatDialogRef<EntryDialogComponent>
-  ) {}
+  ) {
+    this.formBuilder = formBuilder;
+  }
 
   ngOnInit() {
-    this.form = new FormGroup({
-      value: new FormControl('', Validators.required),
-      timestamp: new FormControl(new Date()),
+    this.form = this.formBuilder.group<FormEntry>({
+      date: [new Date(), Validators.required],
+      value: ['', Validators.required],
     });
     this.addAnotherControl = new FormControl(false);
   }
@@ -28,13 +38,13 @@ export class EntryDialogComponent implements OnInit {
   async onSubmit() {
     const formValue = this.form.value;
     if (this.addAnotherControl.value) {
-      this.form.reset({ value: '', timestamp: formValue.timestamp });
+      this.form.reset({ ...formValue, value: '' });
     } else {
       this.dialogRef.close();
     }
     await this.entryService.addEntry({
       value: +formValue.value,
-      timestamp: formValue.timestamp,
+      timestamp: formValue.date,
     });
   }
 }
