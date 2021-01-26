32b594a46db5266f64130545857875b274ef46bb
Benjamin Hogl <benjamin.hogl@jambit.com>
2021-01-26 18:01:52 +0100

    Add time input

--- trackit/src/app/entry/entry-dialog/entry-dialog.component.ts
+++ trackit/src/app/entry/entry-dialog/entry-dialog.component.ts
@@ -6,6 +6,7 @@ import { EntryService } from 'src/app/services/entry.service';
 
 interface FormEntry {
   date: Date;
+  time: string;
   value: string;
 }
 
@@ -30,21 +31,33 @@ export class EntryDialogComponent implements OnInit {
   ngOnInit() {
     this.form = this.formBuilder.group<FormEntry>({
       date: [new Date(), Validators.required],
+      time: [getTimeStringFromDate(new Date()), Validators.required],
       value: ['', Validators.required],
     });
     this.addAnotherControl = new FormControl(false);
   }
 
   async onSubmit() {
-    const formValue = this.form.value;
+    const formValues = this.form.value;
+    const value = +formValues.value;
+    const timestamp = formValues.date;
+    setTimeStringToDate(formValues.time, timestamp);
+
     if (this.addAnotherControl.value) {
-      this.form.reset({ ...formValue, value: '' });
+      this.form.reset({ ...formValues, value: '' });
     } else {
       this.dialogRef.close();
     }
-    await this.entryService.addEntry({
-      value: +formValue.value,
-      timestamp: formValue.date,
-    });
+    await this.entryService.addEntry({ value, timestamp });
   }
 }
+
+const getTimeStringFromDate = (d: Date) =>
+  [d.getHours(), d.getMinutes(), d.getSeconds()]
+    .map((x) => x.toString().padStart(2, '0'))
+    .join(':');
+
+const setTimeStringToDate = (t: string, d: Date) => {
+  const [h, m, s] = t.split(':');
+  d.setHours(+h, +m, +s);
+};
